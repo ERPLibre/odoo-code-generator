484675146a65042c00becc78f9ae36b27d9ae70e
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:12:58 -0400

    [MIG] code_generator_hook_migrate_db_servers: Migration to 14.0

--- code_generator_hook_migrate_db_servers/__manifest__.py
+++ code_generator_hook_migrate_db_servers/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Code Generator - Hook migrate DB servers",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "summary": "Code Generator - Create hook migrate DB servers module",
     "description": (
         "Code generator builder to create hook for module installation, with"
