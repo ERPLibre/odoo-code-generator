dac309882bd6263a24721828465e3115f1c607a5
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 01:58:18 -0400

    [MIG] code_generator_geoengine: Migration to 18.0

--- code_generator_geoengine/__manifest__.py
+++ code_generator_geoengine/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Code Generator - geoengine",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "summary": "Code Generator - Manage geoengine",
     "description": "Code generator builder to manage geoengine",
     "author": "Mathben (mathben@technolibre.ca)",
