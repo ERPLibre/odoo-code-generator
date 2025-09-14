2a98dbecd77fbd3621f0c7a8d5f4fe6083c8afa2
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:15:10 -0400

    [MIG] code_generator_portal: Migration to 16.0

--- code_generator_portal/__manifest__.py
+++ code_generator_portal/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Code Generator - Portal generation",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "summary": "Code Generator - Create portal views",
     "description": (
         "Code generator builder to create views of portal application"
