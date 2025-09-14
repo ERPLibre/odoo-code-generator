e1d68221478e195a273d08a54f12f75925bd8edf
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:12:58 -0400

    [MIG] code_generator_website_snippet: Migration to 14.0

--- code_generator_website_snippet/__manifest__.py
+++ code_generator_website_snippet/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Code Generator - website snippet generation",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "summary": "Code Generator - Create website snippet",
     "description": "Code generator builder to create website snippet",
     "author": "Mathben (mathben@technolibre.ca)",
