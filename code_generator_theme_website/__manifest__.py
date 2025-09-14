70317c30ed37ce70889e27d182d467be6d67022a
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:12:58 -0400

    [MIG] code_generator_theme_website: Migration to 14.0

--- code_generator_theme_website/__manifest__.py
+++ code_generator_theme_website/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Code Generator - Theme website",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "summary": "Code Generator - Create theme website module",
     "description": "Code generator builder to create theme website",
     "author": "Mathben (mathben@technolibre.ca)",
