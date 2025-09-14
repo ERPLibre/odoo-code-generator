ec73253d6848f3e93225ae2d28022b46f4821f91
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 01:58:18 -0400

    [MIG] code_generator_website_leaflet: Migration to 18.0

--- code_generator_website_leaflet/__manifest__.py
+++ code_generator_website_leaflet/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Code Generator - website leaflet generation",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "summary": "Code Generator - Create website leaflet",
     "description": "Code generator builder to create website leaflet snippet",
     "author": "Mathben (mathben@technolibre.ca)",
