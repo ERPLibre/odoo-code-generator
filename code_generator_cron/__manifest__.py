a5662889a2c7782c231dd68e1e9449ae6f6cdad1
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 01:58:18 -0400

    [MIG] code_generator_cron: Migration to 18.0

--- code_generator_cron/__manifest__.py
+++ code_generator_cron/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Code Generator - Cron",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "summary": "Code Generator - Create cron module",
     "description": (
         "Code generator builder to create cron for module installation"
