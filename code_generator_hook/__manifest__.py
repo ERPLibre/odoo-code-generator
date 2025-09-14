9b533d70791ef14f6f9ea03a5cde5bfd5566d39e
Mathieu Benoit <mathben@technolibre.ca>
2025-09-14 00:12:58 -0400

    [MIG] code_generator_hook: Migration to 14.0

--- code_generator_hook/__manifest__.py
+++ code_generator_hook/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Code Generator - Hook",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "summary": "Code Generator - Create hook module",
     "description": (
         "Code generator builder to create hook for module installation"
