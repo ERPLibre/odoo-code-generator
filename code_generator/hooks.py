bf0922c30900c54e4bc459fc79129b7bf7d16d5d
Mathieu Benoit <mathben@technolibre.ca>
2025-11-20 11:44:18 -0500

    [IMP] code generator: disable constraint x_ for manual field

--- code_generator/hooks.py
+++ code_generator/hooks.py
@@ -17,3 +17,21 @@ def post_init_hook(env):
     system_user.groups_id = [
         (4, env.ref("code_generator.code_generator_manager").id, False)
     ]
+
+    # DANGEREUX : supprime une contrainte du core Odoo
+    env.cr.execute(
+        """
+        DO $$
+        BEGIN
+          IF EXISTS (
+            SELECT 1
+            FROM information_schema.table_constraints
+            WHERE constraint_name = 'ir_model_fields_name_manual_field'
+              AND table_name = 'ir_model_fields'
+          ) THEN
+            ALTER TABLE ir_model_fields
+            DROP CONSTRAINT ir_model_fields_name_manual_field;
+          END IF;
+        END$$;
+        """
+    )
